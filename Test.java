542af82c6a335682b24ea892a68dad83842ab914
shivaraj926 <101784389+shivaraj926@users.noreply.github.com>
2022-03-17 17:16:31 +0530

    added test prompt

--- Test.java
+++ Test.java
@@ -1,10 +1,20 @@
 package abc1;
 
+import java.lang.String;
+import java.util.Scanner;
+
 public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- System.out.println("hello word!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+public static void main(String []args) {
+	System.out.println("Hello World!!");
+	System.out.println("are you hungry");
+	Scanner scan=new Scanner(System.in);
+	String input=scan.nextLine();
+	if(input.equalsIgnoreCase("yes")) {
+		System.out.println("eat samosa");
 	}
-
+	else {
+		System.out.println("do yourn work");
+	}
+	
+}
 }
